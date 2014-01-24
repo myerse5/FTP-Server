ae929f4c9a3aa9d01e8ba7ae0ffcca73dda8c462
Evan Myers <myerse5@students.cs.macewan.ca>
2014-01-24 12:07:04 -0700

    First pass on file 'users.*' for consistency with the rest of the program files. First push to github.

--- ftp/users.c
+++ ftp/users.c
@@ -19,97 +19,130 @@
 #include "users.h"
 
 
-void cmd_user(session_info_t *si,char *arg) {
-  
+//Local function prototypes.
+static void getMD5 (char *user, char *password, char *md5str);
+
+
+/******************************************************************************
+ * cmd_user - see "users.h"
+ *****************************************************************************/
+void cmd_user (session_info_t *si, char *arg)
+{
+  char *reply;
   //if user command is given, log current user out
   if (arg == NULL) {
-    char *noarg = "501 Syntax error in parameters or arguments.\n";
-    send_all(si->c_sfd, (uint8_t*)noarg, strlen(noarg));
+    reply = "501 Syntax error in parameters or arguments.\n";
+    send_all (si->c_sfd, (uint8_t*)reply, strlen (reply));
     return;
   }
   si->logged_in = false;
   si->user[0] = '\0';
   
-  //check if user is anonymous
-  if (strcasecmp(arg,"anonymous") == 0) {
+  //Check if user is anonymous. Anonymous user requires no password.
+  if (strcasecmp (arg, "anonymous") == 0) {
     si->logged_in = true;
     
-    char *loggedin = "230 Login successful.\n";
-    send_all(si->c_sfd, (uint8_t*)loggedin, strlen(loggedin));
+    reply = "230 Login successful.\n";
+    send_all (si->c_sfd, (uint8_t*)reply, strlen (reply));
     
   } else if (arg != NULL) {
-    /* command will take any argument as a valid username in order
+    /* Command will take any argument as a valid username in order
      * to prevent a malicious client from collecting usernames
      * from server.
      */
-    char *needpass = "331 User name okay, need password.\n";
-    send_all(si->c_sfd, (uint8_t*)needpass, strlen(needpass));
+    reply = "331 User name okay, need password.\n";
+    send_all (si->c_sfd, (uint8_t*)reply, strlen (reply));
   }
   //as long as argument isn't null, copy the string over
-  strcpy(si->user,arg);
+  strcpy (si->user, arg);
   return;
 }
 
-void cmd_pass(session_info_t *si, char *cmd) {
-  
-  char *password = NULL;
-  char *notfound = "530 Not logged in.\n";
+
+/******************************************************************************
+ * cmd_pass - see "users.h"
+ *****************************************************************************/
+void cmd_pass (session_info_t *si, char *arg)
+{  
+  char *pass = NULL;
   char md5string[33];
-  
+  char *reply;
+ 
   //if user is logged in, no pass require
   if (si->logged_in) {
-    char *loggedin = "230 Already logged in.\n";
-    send_all(si->c_sfd, (uint8_t*)loggedin, strlen(loggedin));
+    reply = "230 Already logged in.\n";
+    send_all (si->c_sfd, (uint8_t*)reply, strlen (reply));
     return;
   }
   
-  
-  //check if username has been given
+  //Check if a username has been given.
   if (strlen(si->user) > 0) {
-    if (cmd) {
-      if ((password = get_config_value (si->user,USER_CONFIG_FILE)) == NULL) {
-	send_all (si->c_sfd, (uint8_t*)notfound, strlen(notfound));
+    if (arg) {
+      if ((pass = get_config_value (si->user, USER_CONFIG_FILE)) == NULL) {
+	reply = "530 Not logged in.\n";
+	send_all (si->c_sfd, (uint8_t*)reply, strlen (reply));
       } else {
 	//get md5 of password + username
-	getMD5(si->user,cmd,md5string);
-	if (strcmp(md5string,password) == 0) {
-	  char *loggedin = "230 Login successful.\n";
+	getMD5 (si->user, arg, md5string);
+	if (strcmp (md5string, pass) == 0) {
+	  reply = "230 Login successful.\n";
 	  si->logged_in = true;
-	  send_all(si->c_sfd, (uint8_t*)loggedin, strlen (loggedin));
+	  send_all (si->c_sfd, (uint8_t*)reply, strlen (reply));
 	} else {
-	  //found name but password didn't match
-	  send_all(si->c_sfd, (uint8_t*)notfound, strlen (notfound));
+	  //The name was found but the password did not match.
+	  reply = "530 Not logged in.\n";
+	  send_all (si->c_sfd, (uint8_t*)reply, strlen (reply));
 	}
       }
     } else {
-      //no argument was given
-      char *syntaxerror = "501 Syntax error in arguments.\n";
-      send_all(si->c_sfd, (uint8_t*)syntaxerror, strlen (syntaxerror));
+      //No argument was given, the USER command fails.
+      reply = "501 Syntax error in arguments.\n";
+      send_all (si->c_sfd, (uint8_t*)reply, strlen (reply));
       return;
     }
   } else {
-    char *badsequence = "503 Login with USER first.\n";
-    send_all(si->c_sfd, (uint8_t*)badsequence, strlen (badsequence));
+    reply = "503 Login with USER first.\n";
+    send_all (si->c_sfd, (uint8_t*)reply, strlen (reply));
   }
   
   if (password)
-    free(password);
+    free (password);
   
   return;
 }
 
-//TODO add function header
-int getMD5 (char *user, char *password, char *md5string) {
-  //TODO fix the buffer overruns in this block
+
+/******************************************************************************
+ * Convert a PASS argument to an MD5 to be compared with the password found in
+ * "users.conf".
+ *
+ * Arguments:
+ *   user - The currently logged in username.
+ *   password - The argument given with the PASS command.
+ *   md5str - The string to contain the md5 string.
+ *
+ * Original author: Justin Slind
+ * Updated by: Evan Myers
+ *****************************************************************************/
+static void getMD5 (char *user, char *pass, char *md5str)
+{
+  /* TODO fix the buffer overruns in this block. Username could be greater than
+   * 500 characters, password could be longer than 500 characters, both
+   * combined may definately be longer than 500 characters. */
   char data[500];
   byte_t md5checksum[16];
-  strcpy (data,password);
-  strcat (data,user);
   struct md5CTX md5struct;
+
+  //Salt the password with the username before creating the checksum.
+  strcpy (data, pass);
+  strcat (data, user);
+
+  //Build the checksum.
   md5Start (&md5struct);
-  md5Add (&md5struct,(byte_t*)data,strlen(data));
-  md5End (&md5struct,md5checksum);
+  md5Add (&md5struct, (byte_t*)data, strlen (data));
+  md5End (&md5struct, md5checksum);
+
+  //Create the checksum string.
   for(int i = 0; i < 16; ++i)
-    sprintf (&md5string[i*2], "%02x", (unsigned int)md5checksum[i]);
-  return 0;
+    sprintf (&md5str[i*2], "%02x", (unsigned int)md5checksum[i]);
 }
--- ftp/users.h
+++ ftp/users.h
@@ -16,48 +16,33 @@
 #include "md5.h"
 
 /******************************************************************************
- * Check if username is valid
+ * Logs the client onto the server with a username in preparation for the PASS
+ * command.
  *
  * Arguments:
- *   si - info for current session
- *   cmd - current command with parameter
- *
- * Returns
- * 	void
+ *    si - Info for the current session.
+ *   arg - The username argument.
  *
  * Original author: Justin Slind
+ * Updated by: Evan Myers
  *****************************************************************************/
-void cmd_user(session_info_t *si,char *cmd);
+void cmd_user (session_info_t *si, char *arg);
 
 
 /******************************************************************************
- * checks if password matches for the previously given user
+ * Finish the login procedure for the client. The PASS command must be sent to
+ * the server immediately after the USER command. If the argument (arg)
+ * passed to this function does not match the password found in 'users.conf'
+ * for the given user, the login procedure will not complete and the client
+ * must start again.
  *
  * Arguments:
- *   si - info for current session
- *   cmd - current command with parameter
- *
- * Returns
- * 	void
+ *    si - Info for the current session.
+ *   arg - The password argument.
  *
  * Original author: Justin Slind
  *****************************************************************************/
-void cmd_pass(session_info_t *si,char *cmd);
+void cmd_pass (session_info_t *si, char *cmd);
 
-
-/******************************************************************************
- * finds an md5 based on the provided password and the username
- *
- * Arguments:
- *   user - username string
- *   password - password string
- *   md5checksum - string to hold checksum
- *
- * Returns
- * 	0
- *
- * Original author: Justin Slind
- *****************************************************************************/
-int getMD5(char *user, char *password, char md5checksum[]);
 
 #endif //__USERS_H__
